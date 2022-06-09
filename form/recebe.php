b098459c8e1b388ef742ac3cc69421017b51878c
Ariel Martins <arielmartinsalvessilva@gmail.com>
2022-06-09 14:10:15 -0300

    content

---
+++ form/recebe.php
@@ -0,0 +1,18 @@
+﻿<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Confirmação</title>
+        </head>
+
+<body>
+
+<?php 
+
+echo $_POST['name']; 
+
+?>
+
+</body>
+
+</html>
---
+++ formularioespecial/recebe-formulario.php
@@ -0,0 +1,31 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cadastro Concluído</title>
+</head>
+
+<body>
+
+<?php 
+
+echo $_POST['nome']."<br>";
+echo $_POST['email']."<br>";
+echo $_POST['telefone']."<br>";
+echo $_POST['site']."<br>";
+echo $_POST['datanasc']."<br>";
+echo $_POST['filhos']."<br>";
+echo $_POST['temp']."<br>";
+echo $_POST['hora']."<br>";
+echo $_POST['senha']."<br>";
+echo $_POST['discradio']."<br>";
+echo $_POST['cor']."<br>";
+echo $_POST['mensagem']."<br>";
+echo $_POST['newsletter']."<br>";
+echo $_POST['parceiros']."<br>";
+ 
+
+?>
+
+</body>
+</html>
